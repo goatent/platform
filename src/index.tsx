3b92a3ece432cf5b9e6ec8c38dc1e8370000b10f
Guðmundur Egill <geb12@hi.is>
2019-08-23 20:19:26 +0000

    add GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React, { Suspense } from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app/App';
 import { Normalize } from 'styled-normalize';
+import GlobalStyles from './app/styles/GlobalStyles';
 
 ReactDOM.render(
     <Suspense fallback={<div />}>
         <Normalize />
+        <GlobalStyles />
         <App />
     </Suspense>,
     document.getElementById('goat-react-id')
