67ffc25f924dbd4013ee2ab7e11686809f723472
Guðmundur Egill <geb12@hi.is>
2019-08-23 20:57:41 +0000

    Add some basic folder structure

---
+++ src/app/components/ReusableComponent/ReusableComponent.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+interface IReusableComponentProps {}
+
+const ReusableComponent: React.FunctionComponent<IReusableComponentProps> = props => {
+    return null;
+};
+export default ReusableComponent;
